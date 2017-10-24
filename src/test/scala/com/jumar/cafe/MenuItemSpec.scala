3520b3b09d7c66c12151782e550c60b2d2414e9d
dsp02 <33044590+dsp02@users.noreply.github.com>
2017-10-24 10:05:40 +0100

    Add test for Cola as a menu item

---
+++ src/test/scala/com/jumar/cafe/MenuItemSpec.scala
@@ -0,0 +1,20 @@
+package com.jumar.cafe
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Created by Derek on 24/10/2017.
+  */
+class MenuItemSpec extends FlatSpec with Matchers {
+
+  "Cola drink" should "be an item on the menu" in {
+    assert(Cola.name == "Cola")
+  }
+  it should "be cold" in {
+    assert(Cola.isHot == false)
+  }
+  it should "be priced at 50p" in {
+    assert(Cola.price == 0.50)
+  }
+
+}
