380cd487ec4c6ec1c6e757f15c67c5cb01bfe046
dsp02 <33044590+dsp02@users.noreply.github.com>
2017-10-24 20:07:29 +0100

    Added extra tests for items not on the menu.

--- src/test/scala/com/jumar/cafe/ProcessOrderSpec.scala
+++ src/test/scala/com/jumar/cafe/ProcessOrderSpec.scala
@@ -19,13 +19,18 @@ class ProcessOrderSpec extends FlatSpec with Matchers {
   it should "have a cost of £2.20 for a Steak Sandwich" in {
     assert(Order.process(Array[String]("Steak Sandwich")) == 5.40)
   }
-  it should "have a price of 0.00 for an empty string order" in {
-
+  it should "have a cost of £1.50 for a Cola and a Coffee with a unexplained item " in {
+    assert(Order.process(Array[String]("Cola", "", "Coffee")) == 1.50)
+  }
+  it should "not give a price for Pasta as not on the menu, £1.50 for a Cola and a Coffee." in {
+    assert(Order.process(Array[String]("Cola", "Pasta", "Coffee")) == 1.50)
+  }
+  it should "not be able to process an empty string order" in {
     assertThrows[IllegalArgumentException] {
       (Order.process(Array[String]("")))
     }
   }
-  it should "have a price of 0.00 for an empty order" in {
+  it should "not be able to process a null order" in {
     assertThrows[IllegalArgumentException](Order.process(Array[String]()))
   }
 }
