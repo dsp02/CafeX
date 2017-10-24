1382ddfa3336f44defb2fb072b4f657a027b06e2
dsp02 <33044590+dsp02@users.noreply.github.com>
2017-10-24 10:43:05 +0100

    Added other menu items.
    Coffee, Cheese Sandwich & Steak Sandwich added.

--- src/main/scala/com/jumar/cafe/MenuItem.scala
+++ src/main/scala/com/jumar/cafe/MenuItem.scala
@@ -3,10 +3,12 @@ package com.jumar.cafe
 /**
   * Created by Derek on 24/10/2017.
   */
-trait MenuItem {
+sealed trait MenuItem {
   def name: String
   def isHot: Boolean
   def price: BigDecimal
+
+  override def toString: String = s"$name, $isHot, $price"
 }
 
 case object Cola extends MenuItem {
@@ -15,4 +17,28 @@ case object Cola extends MenuItem {
   override def isHot = false
 
   override def price = 0.5
+}
+
+case object Coffee extends MenuItem {
+  override def name = "Coffee"
+
+  override def isHot = true
+
+  override def price = 1.00
+}
+
+case object CheeseSandwich extends MenuItem {
+  override def name = "Cheese Sandwich"
+
+  override def isHot = false
+
+  override def price = 2.00
+}
+
+case object SteakSandwich extends MenuItem {
+  override def name = "Steak Sandwich"
+
+  override def isHot = true
+
+  override def price = 4.5
 }
