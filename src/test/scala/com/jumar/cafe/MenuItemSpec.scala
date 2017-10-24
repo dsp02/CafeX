714a40fc4fd3dfacb5bd31b017d7a28082b1a84b
dsp02 <33044590+dsp02@users.noreply.github.com>
2017-10-24 10:25:33 +0100

    Added tests for coffee, cheese & steal sandwiches as menu items.

--- src/test/scala/com/jumar/cafe/MenuItemSpec.scala
+++ src/test/scala/com/jumar/cafe/MenuItemSpec.scala
@@ -16,4 +16,35 @@ class MenuItemSpec extends FlatSpec with Matchers {
   it should "be priced at 50p" in {
     assert(Cola.price == 0.50)
   }
+
+  "Coffee drink" should "be an item on the menu" in {
+    assert(Coffee.name == "Coffee")
+  }
+  it should "be hot" in {
+    assert(Coffee.isHot == true)
+  }
+  it should "be priced at £1.00" in {
+    assert(Coffee.price == 1.00)
+  }
+
+  "A Cheese Sandwich" should "be an item on the menu" in {
+    assert(CheeseSandwich.name == "Cheese Sandwich")
+  }
+  it should "be cold" in {
+    assert(CheeseSandwich.isHot == false)
+  }
+  it should "be priced at £2.00" in {
+    assert(CheeseSandwich.price == 2.00)
+  }
+
+  "A Steak Sandwich" should "be an item on the menu" in {
+    assert(SteakSandwich.name == "Steak Sandwich")
+  }
+  it should "be hot" in {
+    assert(SteakSandwich.isHot == true)
+  }
+  it should "be priced at £4.50" in {
+    assert(SteakSandwich.price == 4.50)
+  }
+
 }
